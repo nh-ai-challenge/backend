eee705a8e072ab758995c2147f4a5a4e1f1ef24d
summit1123 <ruggy245@naver.com>
2025-09-17 11:47:20 +0900

    feat: Persona 모델 구현 - 페르소나 타입 및 차원별 점수 정의

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,4 +1,5 @@
 from app.models.user import User, UserType
 from app.models.survey import Survey, SurveyQuestion
+from app.models.persona import Persona, PersonaType
 
-__all__ = ["User", "UserType", "Survey", "SurveyQuestion"]
+__all__ = ["User", "UserType", "Survey", "SurveyQuestion", "Persona", "PersonaType"]
